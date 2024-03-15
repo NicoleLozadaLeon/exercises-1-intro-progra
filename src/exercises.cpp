352239348fd53b194365c49f233b4d55951d4432
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-03-15 19:52:39 -0400

    ex2 seven test :(

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -39,9 +39,11 @@ void exercise_2(double A, double B, double C) {
     } else if (calcular_datos_raiz == 0 ) {
         double calcular_raiz_cero = -B / (2 * A);
         cout << calcular_raiz_cero << endl;
-    } else {
-    cout << "" ;
-  }
+    }  else if (calcular_datos_raiz < 0 ) {
+        cout << "";
+    }
+    else;
+
 }
 
 void exercise_3(int a, int b) {
