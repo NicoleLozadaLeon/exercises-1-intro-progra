fcd2b8e616b9435b09dd5e324ae7ecb945d965f6
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-03-15 19:21:13 -0400

    ex3 quito -inf

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -26,7 +26,7 @@ void exercise_1(string s1, string s2, string s3) {
 
 void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
-    double calcular_datos_raiz = B * B - 4 * A * C;
+     double calcular_datos_raiz = B * B - 4 * A * C;
     if (A == 0) {
        double calcular_ByC = -C / B;
         cout << calcular_ByC << endl;
@@ -36,10 +36,13 @@ void exercise_2(double A, double B, double C) {
         double calcular_positiva_fraccion = (-B + sqrt(calcular_datos_raiz)) / (2 * A);
         double calcular_negativa_fraccion = (-B - sqrt(calcular_datos_raiz)) / (2 * A);
         cout << calcular_positiva_fraccion << " " << calcular_negativa_fraccion << endl;
-    } else if (calcular_datos_raiz == 0) {
+    } else if (calcular_datos_raiz == 0 ) {
         double calcular_raiz_cero = -B / (2 * A);
         cout << calcular_raiz_cero << endl;
-    } else;
+    } else if (calcular_datos_raiz < 0 ) {
+        cout << "";
+    }
+    else;
 }
 
 void exercise_3(int a, int b) {
