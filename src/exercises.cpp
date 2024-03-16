993d3730a6455134fcadd66f6112a2c0b51f8133
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-03-16 18:05:47 -0400

    ex9 pienso como función string

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -168,13 +168,14 @@ void exercise_8(long int seconds) {
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
-  /*
    if (s1[0] == s5[0]) {
-        cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
+      cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
+        return "Hemos encontrado algo!";
     } else {
-        cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
+      cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
+        return "Aun sin suerte";
     }
-  return "";*/
+  return ""; 
 }
 
 int exercise_10(int a, int b) {
