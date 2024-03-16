7275e11a9b7ee3b3da17ef913a9cfdaca6723f52
Nicole <142690531+NicoleLozadaLeon@users.noreply.github.com>
2024-03-16 17:11:16 -0400

    ex9 commit

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -168,12 +168,13 @@ void exercise_8(long int seconds) {
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
+  /*
    if (s1[0] == s5[0]) {
         cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
     } else {
         cout << s1[0] << s2[0] << s3[0] << s4[0] << s5[0] << endl;
     }
-  return "";
+  return "";*/
 }
 
 int exercise_10(int a, int b) {
